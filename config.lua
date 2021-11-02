9c53cfdaf619d33603d84039042c679c9d740cd1
Vu Tran <baysao@gmail.com>
2021-11-02 18:15:13 +0700

    proxy gdnsd via nginx

--- config.lua
+++ config.lua
@@ -9,12 +9,21 @@ local _config = {
     templates = {},
     apps = {},
     supervisor = [[
-]],
-    supervisor1 = [[
 
-[program:gateway_pull]
+[program:gwman_pull]
 command=/bin/bash _SITE_ROOT_/scripts/run loop _pull _SITE_ROOT_ v1
 autorestart=true
+redirect_stderr=true
+stdout_logfile=_SITE_ROOT_/logs/gwman_pull.log
+
+
+[program:gwman_gdnsd]
+command=/bin/bash _SITE_ROOT_/scripts/run loop _service_gdnsd _SITE_ROOT_ v1
+autorestart=true
+redirect_stderr=true
+stdout_logfile=_SITE_ROOT_/logs/gwman_gdnsd.log
+
+
     ]]
 }
 return _config
